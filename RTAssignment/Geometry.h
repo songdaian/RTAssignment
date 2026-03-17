be41b45374b7b99ea4eac4be3cacc082d1315101
songdaian <18921159828@163.com>
2026-03-17 20:40:54 +0000

    Initial commit

---
+++ RTAssignment/Geometry.h
@@ -0,0 +1,262 @@
+#pragma once
+
+#include "Core.h"
+#include "Sampling.h"
+
+class Ray
+{
+public:
+	Vec3 o;
+	Vec3 dir;
+	Vec3 invDir;
+	Ray()
+	{
+	}
+	Ray(Vec3 _o, Vec3 _d)
+	{
+		init(_o, _d);
+	}
+	void init(Vec3 _o, Vec3 _d)
+	{
+		o = _o;
+		dir = _d;
+		invDir = Vec3(1.0f / dir.x, 1.0f / dir.y, 1.0f / dir.z);
+	}
+	Vec3 at(const float t) const
+	{
+		return (o + (dir * t));
+	}
+};
+#define EPSILON 0.001f
+
+class Plane
+{
+public:
+	Vec3 n;
+	float d;
+	void init(Vec3& _n, float _d)
+	{
+		n = _n;
+		d = _d;
+	}
+	// Add code here
+	bool rayIntersect(Ray& r, float& t)
+	{
+		float denom = Dot(n, r.dir);
+		if (fabs(denom) < EPSILON) { return false; }
+		t = -(d + Dot(n, r.o)) / denom;
+		if (t < 0) { return false; }
+		return true;
+	}
+};
+
+
+class Triangle
+{
+public:
+	Vertex vertices[3];
+	Vec3 e1; // Edge 1
+	Vec3 e2; // Edge 2
+	Vec3 n; // Geometric Normal
+	float area; // Triangle area
+	float d; // For ray triangle if needed
+	unsigned int materialIndex;
+	void init(Vertex v0, Vertex v1, Vertex v2, unsigned int _materialIndex)
+	{
+		materialIndex = _materialIndex;
+		vertices[0] = v0;
+		vertices[1] = v1;
+		vertices[2] = v2;
+		e1 = vertices[2].p - vertices[1].p;
+		e2 = vertices[0].p - vertices[2].p;
+		n = e1.cross(e2).normalize();
+		area = e1.cross(e2).length() * 0.5f;
+		d = Dot(n, vertices[0].p);
+	}
+	Vec3 centre() const
+	{
+		return (vertices[0].p + vertices[1].p + vertices[2].p) / 3.0f;
+	}
+	// Add code here
+	bool rayIntersect(const Ray& r, float& t, float& u, float& v) const
+	{
+		//float denom = Dot(n, r.dir);
+		//if (denom == 0) return false;
+		//t = -(Dot(n, r.o) + d) / denom;
+		//if (t < 0) return false;
+		//Vec3 P = r.at(t);
+		//Vec3 q1 = P - vertices[1].p;
+		//Vec3 C1 = Cross(e1, q1);
+		//float invA = 1/Dot(Cross(e1, e2), n);
+		//u = Dot(C1, n) * invA;
+		//if (u < 0 || u>1.f) return false;
+		//Vec3 q2 = P - vertices[2].p;
+		//Vec3 C2 = Cross(e2, q2);
+		//v = Dot(C2, n) * invA;
+		//if (v < 0 || u + v > 1.f) return false;
+		//return true;
+
+
+		float denom = Dot(n, r.dir);
+		if (denom == 0) { return false; }
+		t = (d - Dot(n, r.o)) / denom;
+		if (t < 0) { return false; }
+		Vec3 p = r.at(t);
+		float invArea = 1.0f / Dot(e1.cross(e2), n);
+		u = Dot(e1.cross(p - vertices[1].p), n) * invArea;
+		if (u < 0 || u > 1.0f) { return false; }
+		v = Dot(e2.cross(p - vertices[2].p), n) * invArea;
+		if (v < 0 || (u + v) > 1.0f) { return false; }
+		return true;
+
+		//Vec3 T = r.o - vertices[0].p;
+		//float invdet = 1 / Dot(e1, Cross(r.dir, e2));
+		//float beta = Dot(T, Cross(r.dir, e2)) * invdet;
+		//if (beta < 0 || beta>1) return false;
+		//Vec3 q = Cross(T, e1);
+		//float gamma = Dot(r.dir, q) * invdet;
+		//if (gamma < 0 || beta + gamma>1) return false;
+		//t = Dot(e2, q) * invdet;
+		//return true;
+	}
+	void interpolateAttributes(const float alpha, const float beta, const float gamma, Vec3& interpolatedNormal, float& interpolatedU, float& interpolatedV) const
+	{
+		interpolatedNormal = vertices[0].normal * alpha + vertices[1].normal * beta + vertices[2].normal * gamma;
+		interpolatedNormal = interpolatedNormal.normalize();
+		interpolatedU = vertices[0].u * alpha + vertices[1].u * beta + vertices[2].u * gamma;
+		interpolatedV = vertices[0].v * alpha + vertices[1].v * beta + vertices[2].v * gamma;
+	}
+	// Add code here
+	Vec3 sample(Sampler* sampler, float& pdf)
+	{
+		return Vec3(0, 0, 0);
+	}
+	Vec3 gNormal()
+	{
+		return (n * (Dot(vertices[0].normal, n) > 0 ? 1.0f : -1.0f));
+	}
+};
+
+class AABB
+{
+public:
+	Vec3 max;
+	Vec3 min;
+	AABB()
+	{
+		reset();
+	}
+	void reset()
+	{
+		max = Vec3(-FLT_MAX, -FLT_MAX, -FLT_MAX);
+		min = Vec3(FLT_MAX, FLT_MAX, FLT_MAX);
+	}
+	void extend(const Vec3 p)
+	{
+		max = Max(max, p);
+		min = Min(min, p);
+	}
+	// Add code here
+	bool rayAABB(const Ray& r, float& t)
+	{
+		Vec3 Tmin = (min - r.o) * r.invDir;
+		Vec3 Tmax = (max - r.o) * r.invDir;
+		Vec3 Tentry = Min(Tmin, Tmax);
+		Vec3 Texit = Max(Tmin, Tmax);
+		float tentry = std::max(Tentry.x, std::max(Tentry.y, Tentry.z));
+		float texit = std::min(Texit.x, std::min(Texit.y, Texit.z));
+		//t = std::min(tentry, texit);
+		//return (tentry <= texit && texit > 0);
+		if (tentry > texit || texit < 0)
+			return false;
+
+		t = (tentry >= 0) ? tentry : texit;
+		return true;
+	}
+	// Add code here
+	bool rayAABB(const Ray& r)
+	{
+		Vec3 Tmin = (min - r.o) * r.invDir;
+		Vec3 Tmax = (max - r.o) * r.invDir;
+		Vec3 Tentry = Min(Tmin, Tmax);
+		Vec3 Texit = Max(Tmin, Tmax);
+		float tentry = std::max(Tentry.x, std::max(Tentry.y, Tentry.z));
+		float texit = std::min(Texit.x, std::min(Texit.y, Texit.z));
+		return (tentry <= texit && texit > 0);
+	}
+	// Add code here
+	float area()
+	{
+		Vec3 size = max - min;
+		return ((size.x * size.y) + (size.y * size.z) + (size.x * size.z)) * 2.0f;
+	}
+};
+
+class Sphere
+{
+public:
+	Vec3 centre;
+	float radius;
+	void init(Vec3& _centre, float _radius)
+	{
+		centre = _centre;
+		radius = _radius;
+	}
+	// Add code here
+	bool rayIntersect(Ray& r, float& t)
+	{
+		return false;
+	}
+};
+
+struct IntersectionData
+{
+	unsigned int ID;
+	float t;
+	float alpha;
+	float beta;
+	float gamma;
+};
+
+#define MAXNODE_TRIANGLES 8
+#define TRAVERSE_COST 1.0f
+#define TRIANGLE_COST 2.0f
+#define BUILD_BINS 32
+
+class BVHNode
+{
+public:
+	AABB bounds;
+	BVHNode* r;
+	BVHNode* l;
+	// This can store an offset and number of triangles in a global triangle list for example
+	// But you can store this however you want!
+	// unsigned int offset;
+	// unsigned char num;
+	BVHNode()
+	{
+		r = NULL;
+		l = NULL;
+	}
+	// Note there are several options for how to implement the build method. Update this as required
+	void build(std::vector<Triangle>& inputTriangles)
+	{
+		// Add BVH building code here
+	}
+	void traverse(const Ray& ray, const std::vector<Triangle>& triangles, IntersectionData& intersection)
+	{
+		// Add BVH Traversal code here
+	}
+	IntersectionData traverse(const Ray& ray, const std::vector<Triangle>& triangles)
+	{
+		IntersectionData intersection;
+		intersection.t = FLT_MAX;
+		traverse(ray, triangles, intersection);
+		return intersection;
+	}
+	bool traverseVisible(const Ray& ray, const std::vector<Triangle>& triangles, const float maxT)
+	{
+		// Add visibility code here
+		return true;
+	}
+};
